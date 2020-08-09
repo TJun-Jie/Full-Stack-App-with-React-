53cd35bee81dcfe9fa5ffe1dc5e4c39d82615010
TJun-Jie <tjunjie2000@gmail.com>
2020-08-10 00:36:09 +0800

    Added signin comoponent functionality

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function UserSignIn() {
-    return (
-        <div className="bounds">
-        <div className="grid-33 centered signin">
-          <h1>Sign In</h1>
-          <div>
-            <form>
-              <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value=""/></div>
-              <div><input id="password" name="password" type="password" className="" placeholder="Password" value=""/></div>
-              <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-            </form>
-          </div>
-          <p>&nbsp;</p>
-          <p>Don't have a user account? <a href="sign-up.html">Click here</a> to sign up!</p>
+  const [emailAddress,setEmailAddress] = useState('');
+  const [password,setPassword] = useState('');
+  return (
+      <div className="bounds">
+      <div className="grid-33 centered signin">
+        <h1>Sign In</h1>
+        <div>
+          <form>
+            <div><input
+              id="emailAddress" 
+              name="emailAddress" 
+              type="text" 
+              className="" 
+              placeholder="Email Address" 
+              value={emailAddress}
+              onChange = {e => setEmailAddress(e.target.value)}
+              /></div>
+            <div><input 
+              id="password" 
+              name="password" 
+              type="password" 
+              className="" 
+              placeholder="Password" 
+              value={password}
+              onChange = {e => setPassword(e.target.value)}
+              /></div>
+            <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><Link to="/" className="button button-secondary">Cancel</Link></div>
+          </form>
         </div>
+        <p>&nbsp;</p>
+        <p>Don't have a user account? <a href="sign-up.html">Click here</a> to sign up!</p>
       </div>
-    )
+    </div>
+  )
 }
 
 export default UserSignIn;
