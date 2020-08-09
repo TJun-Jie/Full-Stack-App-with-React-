d235004f92009eb26869239b9acfd099c229a2f2
TJun-Jie <tjunjie2000@gmail.com>
2020-08-09 17:23:58 +0800

    Added react router and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail'
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
 
 function App() {
 
   return (
-    <div>
-      <Header></Header>
-      <hr></hr>
-      {/* <Courses></Courses> */}
-      {/* <CourseDetail></CourseDetail> */}
-      {/* <CreateCourse></CreateCourse> */}
-      <UpdateCourse></UpdateCourse>
-    </div>
+    <Router>
+      <div>
+        <Header></Header>
+        <hr></hr>
+        <Switch>
+          <Route path="/courses/create">
+            <CreateCourse></CreateCourse>
+          </Route>
+          <Route path="/courses/:id/update">
+            <UpdateCourse></UpdateCourse>
+          </Route>
+          <Route path="/courses/:id">
+            <CourseDetail></CourseDetail>
+          </Route>
+          <Route path="/signin">
+            <UserSignIn></UserSignIn>
+          </Route>
+          <Route path="/signup">
+            <UserSignUp></UserSignUp>
+          </Route>
+          <Route path="/">
+            <Courses></Courses>
+          </Route>
+        </Switch>
+        
+
+      </div>
+
+    </Router>
     
   );
 }
