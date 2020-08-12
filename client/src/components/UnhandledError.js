29965af7edaf41724628569c92bdcd689b66103e
TJun-Jie <tjunjie2000@gmail.com>
2020-08-12 17:12:09 +0800

    Added notfound , forbidden and unhandled error components

---
+++ client/src/components/UnhandledError.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UnhandledError(){
+    return(
+        <div className="bounds">
+        <h1>Unhandled Error</h1>
+        <p>Sorry! Something went wrong!</p>
+      </div>
+    )
+}
+
+export default UnhandledError;
